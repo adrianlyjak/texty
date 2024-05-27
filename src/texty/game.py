37623b1567884dd7a69976a557cb8930fe488fe1
Adrian Lyjak <adrian@chatmeter.com>
2024-05-27 11:54:50 -0400

    futz

--- src/texty/game.py
+++ src/texty/game.py
@@ -5,10 +5,34 @@ class Message(TypedDict):
     role: Literal["user", "assistant"]
     content: str
 
-class PrewritingState(TypedDict):
+class InternalState(TypedDict):
+    """
+    Internal state and prewriting of the language models story objectives
+    """
     objectives: List[str]
     environment: List[str]
 
 class GameState(TypedDict):
+    """
+    All the game state
+    """
     description: str
     log: List[Message]
+    internal: InternalState
+
+
+class GameProtocol():
+    state: GameState
+    
+    def get_inventory() -> List[str]:
+        pass
+
+    def put_action(action: str) -> Message:
+        pass
+
+    def get_valid_actions() -> List[str]:
+        pass
+    
+    def get_location() -> str:
+        pass
+
